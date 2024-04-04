289073fd92093df8e94f200e636b66116de30a79
Anirudha0721 <anirudha@rhythmflows.com>
2024-04-04 10:10:04 +0530

    add:working with auth

--- app/authconfig.js
+++ app/authconfig.js
@@ -1,4 +1,9 @@
 export const authConfig = {
+  env: {
+    SECRET: process.env.SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+
   providers:[],
   pages: {
     signIn: "/login",
@@ -7,12 +12,12 @@ export const authConfig = {
     authorized({ auth, request }) {
       const isLoggedIn = auth?.user;
       const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false;
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", request.nextUrl));
-      }
+      // if (isOnDashboard) {
+      //   if (isLoggedIn) return true;
+      //   return false;
+      // } else if (isLoggedIn) {
+      //   return Response.redirect(new URL("/dashboard", request.nextUrl));
+      // }
       return true;
     },
   },
--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -79,44 +79,46 @@ const menuItems = [
 ];
 
 const Sidebar = async () => {
-  const { user } = await auth();
+  // const { user } = await auth();
   return (
-    <div className={styles.container}>
-      <div className={styles.user}>
-        <Image
-          className={styles.userImage}
-          src={user.img || "/noavatar.png"}
-          alt=""
-          width="50"
-          height="50"
-        />
-        <div className={styles.userDetail}>
-          <span className={styles.username}>{user.username}</span>
-          <span className={styles.userTitle}>Administrator</span>
-        </div>
-      </div>
-      <ul className={styles.list}>
-        {menuItems.map((cat) => (
-          <li key={cat.title}>
-            <span className={styles.cat}>{cat.title}</span>
-            {cat.list.map((item) => (
-              <MenuLink item={item} key={item.title} />
-            ))}
-          </li>
-        ))}
-      </ul>
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button className={styles.logout}>
-          <MdLogout />
-          Logout
-        </button>
-      </form>
-    </div>
+    <h1>tfrcygvbhjnkml</h1>
+      
+    //  <div className={styles.container}>
+    //   <div className={styles.user}>
+    //     <Image
+    //       className={styles.userImage}
+    //       src={user.img || "/noavatar.png"}
+    //       alt=""
+    //       width="50"
+    //       height="50"
+    //     />
+    //     <div className={styles.userDetail}>
+    //       <span className={styles.username}>{user.username}</span>
+    //       <span className={styles.userTitle}>Administrator</span>
+    //     </div>
+    //   </div>
+    //   <ul className={styles.list}>
+    //     {menuItems.map((cat) => (
+    //       <li key={cat.title}>
+    //         <span className={styles.cat}>{cat.title}</span>
+    //         {cat.list.map((item) => (
+    //           <MenuLink item={item} key={item.title} />
+    //         ))}
+    //       </li>
+    //     ))}
+    //   </ul>
+    //   <form
+    //     action={async () => {
+    //       "use server";
+    //       await signOut();
+    //     }}
+    //   >
+    //     <button className={styles.logout}>
+    //       <MdLogout />
+    //       Logout
+    //     </button>
+    //   </form>
+    // </div> 
   );
 };
 
